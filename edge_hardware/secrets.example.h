71ee408f3e72694952aee4b4076bc25a4a26543e
Abhinaykalal <abhinaykalal019@gmail.com>
2026-09-17 00:21:56 +0530

    chore: add safe ESP32 secrets template

---
+++ edge_hardware/secrets.example.h
@@ -0,0 +1,9 @@
+// Copy this file to secrets.h on the ESP32 development machine.
+// NEVER commit secrets.h.
+#pragma once
+
+#define AGRISAATHI_DEVICE_ID "ESP32_NODE_01"
+#define AGRISAATHI_WIFI_SSID ""
+#define AGRISAATHI_WIFI_PASSWORD ""
+#define AGRISAATHI_MQTT_HOST ""
+#define AGRISAATHI_MQTT_PORT 8883
